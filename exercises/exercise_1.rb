94be6ccbb61cfd2bebc99e2356e2e1562d3fb2eb
shovon231 <shovon231@gmail.com>
2023-09-18 11:17:45 -0400

    adding the exercise_1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,18 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# Create the first store in Burnaby
+Store.create(name: 'Burnaby', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
+
+# Create the second store in Richmond
+Store.create(name: 'Richmond', annual_revenue: 1_260_000, mens_apparel: false, womens_apparel: true)
+
+# Create the third store in Gastown
+Store.create(name: 'Gastown', annual_revenue: 190_000, mens_apparel: true, womens_apparel: false)
+
+puts "Number of stores: #{Store.count}"
+
+
+
+
+
